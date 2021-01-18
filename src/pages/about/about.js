f05e6b825f1838f0ff81933d0b3e7d58585beb03
MapleJack <37164788+MapleJack@users.noreply.github.com>
2021-01-19 01:49:51 +0800

    add function and multi-page
    add multi-page settings

---
+++ src/pages/about/about.js
@@ -0,0 +1,27 @@
+// css
+
+// 共用样式
+import '../../assets/styles/base.css'
+import '../../assets/styles/reset.css'
+
+//字体图标
+import '../../assets/styles/iconfont.css'
+
+
+// bootstrap
+import '../../api/bootstrap4/css/bootstrap.min.css'
+
+
+//js
+
+//字体图标
+import '../../api/js/iconfont.js'
+
+// bootstrap
+import '../../api/bootstrap4/js/jquery.min.js'
+import '../../api/bootstrap4/js/popper.min.js'
+import '../../api/bootstrap4/js/bootstrap.min.js'
+
+// components
+import '../../common/footer/footer.js'
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,64 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+// 引入css 单独打包插件
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 // 获取绝对路径
 const resolve = dir=>path.resolve(__dirname, dir);
 
+// 避免CSS重复复制
+function recursiveIssuer(m, c) {
+    const issuer = c.moduleGraph.getIssuer(m);
+    // For webpack@4 chunks = m.issuer
+
+    if (issuer) {
+        return recursiveIssuer(issuer, c);
+    }
+
+    const chunks = c.chunkGraph.getModuleChunks(m);
+    // For webpack@4 chunks = m._chunks
+
+    for (const chunk of chunks) {
+        return chunk.name;
+    }
+
+    return false;
+}
+
 module.exports = {
-  devServer: {
-    port: 9000
-    },
   mode:"development",
   entry: {
-    index:'./src/pages/index/index.js'
+    index: path.resolve(__dirname, 'src/pages/index/index.js'),
+    about: path.resolve(__dirname, 'src/pages/about/about.js')
   },
   output: {
-    path: resolve('dist'),
+    path: path.resolve(__dirname, 'dist'),
     filename: 'js/[name].js'
   },
   // 调试用，出错时直接定位到原始代码，而不是转换后的代码
   devtool:'cheap-module-eval-source-map',
+    //   optimization: {
+//     splitChunks: {
+//       cacheGroups: {
+//         indexStyles: {
+//           name: 'styles_index',
+//           test: (m, c, entry = 'index') =>
+//             m.constructor.name === 'CssModule' &&
+//             recursiveIssuer(m, c) === entry,
+//           chunks: 'all',
+//           enforce: true,
+//         },
+//         aboutStyles: {
+//           name: 'styles_about',
+//           test: (m, c, entry = 'about') =>
+//             m.constructor.name === 'CssModule' &&
+//             recursiveIssuer(m, c) === entry,
+//           chunks: 'all',
+//           enforce: true,
+//         },
+//       },
+//     },
+//   },
   resolve:{
     // 自动补全（可以省略）的扩展名
     extensions:['.js'],
@@ -57,7 +98,7 @@ module.exports = {
         }
     }, {
         test: /\.css$/,
-        use: [ 'style-loader', 'css-loader' ]
+        use: [ MiniCssExtractPlugin.loader, 'css-loader' ],
     },{
         test: /\.(woff2?|eot|ttf|otf)$/,
         loader: 'url-loader',
@@ -72,6 +113,14 @@ module.exports = {
             filename:'index.html',
             template:'./src/pages/index/index.art',
             chunks:['index']
+        }),
+        new HtmlWebpackPlugin({
+            filename:'about.html',
+            template:'./src/pages/about/about.art',
+            chunks:['about']
+        }),
+        new MiniCssExtractPlugin({
+            filename: ({ chunk }) => `css/${chunk.name.replace('/js/', '/css/')}.css`,
         })
     ]
 };
